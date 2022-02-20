ef6d5bfb96db4a53bd1a2e721fc8387491b9cfde
Elenaaab <ellelaby@gmail.com>
2022-02-20 11:18:15 +0100

    CSS fundations & strategy on Form component

--- my-app/src/Bed/Bed.tsx
+++ my-app/src/Bed/Bed.tsx
@@ -4,15 +4,18 @@ import { BedPropsTypes } from '../type';
 import './Bed.scss';
 
 // Ici on va passer pas mal de props depuis form qui vont gérer les conditions du css : 
+// Les oreillers en conditionnel
+// La couette en conditionnelle, soit on fait un linear gradient soit on a la divise en 12 parts...
 export const Bed: React.FC<BedPropsTypes> = () => {
   return (
     <div className="Bed__container">
       <div className="Bed__head">
-      <div className='Bed__pillow'>Oreiller 1</div>
-      <div className='Bed__pillow'>Oreiller 2</div>
+      <div className="Bed__pillow">Oreiller 1</div>
+      <div className="Bed__pillow">Oreiller 2</div>
       </div>
-      <div className='Bed__sheet'>Bed Sheet
+      <div className="Bed__sheet">Bed Sheet
       <div className="Bed__sheet--fold">Bed Sheet Fold</div>
+      <span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span>
       </div>
     </div>
   );
--- my-app/src/Form/Form.tsx
+++ my-app/src/Form/Form.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FormPropsTypes } from '../type';
 import { Field } from './Field/Field';
+
 import './Form.scss';
 
 
@@ -8,31 +9,37 @@ export const Form: React.FC<FormPropsTypes> = () => {
   return (
     <div>
       <h3>Renseigne tes infos ici :</h3>
-      <form>
-        <label>Ton poids</label>
-        <Field value={''} type={undefined} name={'weight1'} placeholder={'poids en kg'} manageChange={function (): void {
-          throw new Error('Function not implemented.');
-        } } />
-        <label>Ta taille</label>
-        <Field value={''} type={undefined} name={'height1'} placeholder={'taille en cm'} manageChange={function (): void {
-          throw new Error('Function not implemented.');
-        } } />
-        <label>Son poids</label>
-        <Field value={''} type={undefined} name={'weight2'} placeholder={'poids en kg'} manageChange={function (): void {
-          throw new Error('Function not implemented.');
-        } } />
-        <label>Sa taille</label>
-        <Field value={''} type={undefined} name={'height2'} placeholder={'taille en cm'} manageChange={function (): void {
-          throw new Error('Function not implemented.');
-        } } />
-        <label>Les dimensions du lit</label>
-        <Field value={''} type={undefined} name={'bedSizeWeight'} placeholder={'largeur en cm'} manageChange={function (): void {
-          throw new Error('Function not implemented.');
-        } } />
-        <span>X</span>
-        <Field value={''} type={undefined} name={'bedSizeHeight'} placeholder={'longueur en cm'} manageChange={function (): void {
-          throw new Error('Function not implemented.');
-        } } />
+      <form className="Form">
+        <div className="Form__row">
+          <label>Ton poids</label>
+          <Field value={''} type={undefined} name={'weight1'} placeholder={'poids en kg'} manageChange={function (): void {
+            throw new Error('Function not implemented.');
+          } } />
+          <label>Ta taille</label>
+          <Field value={''} type={undefined} name={'height1'} placeholder={'taille en cm'} manageChange={function (): void {
+            throw new Error('Function not implemented.');
+          } } />
+        </div>
+        <div className="Form__row">
+          <label>Son poids</label>
+          <Field value={''} type={undefined} name={'weight2'} placeholder={'poids en kg'} manageChange={function (): void {
+            throw new Error('Function not implemented.');
+          } } />
+          <label>Sa taille</label>
+          <Field value={''} type={undefined} name={'height2'} placeholder={'taille en cm'} manageChange={function (): void {
+            throw new Error('Function not implemented.');
+          } } />
+        </div>
+        <div className="Form__row">
+          <label>Les dimensions du lit</label>
+          <Field value={''} type={undefined} name={'bedSizeWeight'} placeholder={'largeur en cm'} manageChange={function (): void {
+            throw new Error('Function not implemented.');
+          } } />
+          <span>X</span>
+          <Field value={''} type={undefined} name={'bedSizeHeight'} placeholder={'longueur en cm'} manageChange={function (): void {
+            throw new Error('Function not implemented.');
+          } } />
+        </div>
       </form>
     </div>
   );
